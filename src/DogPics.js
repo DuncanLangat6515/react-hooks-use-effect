8d90695fee78dfe7fb76d2a595ae4e20cb642ef1
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-09-26 07:17:46 +0300

    completed assignment

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
@@ -13,7 +15,7 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  }, []);
 
   console.log("render");
 
